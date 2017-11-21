b4b14fd31468cd567581e28c5c1bf41e266ea43a
Heoju <heojh93@naver.com>
2017-11-22 06:31:40 +0900

    sth wrong with arcus build

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+# Import from different folder
+import sys
+sys.path.insert(0, './arcus-python2-client')
+
+# Import Arcus
+from arcus import *
+from arcus_mc_node import ArcusMCNodeAllocator
+from arcus_mc_node import EflagFilter
+
+# Import flask
 from flask import Flask
 from flask import render_template
 
 from db_setting import *
-import MySQLdb
+
+client = Arcus(ArcusLocator(ArcusMCNodeAllocator(ArcusTranscoder())))
+client.connect("172.17.0.3:2181", "rou91-cloud")
+
+# test
+ret = client.set('test:string1', 'test...', 10)
+print ret.get_result()
+assert ret.get_result() == True
 
 # close & delete DB
 cleanDB()
